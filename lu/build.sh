2641e1be05897e8356ae0af7dccba592fda638c8
awsdert <gb2985@gmail.com>
2019-02-06 08:34:33 +0000

    Refinements, struggling to compile on my system, might be a data issue as I had to reinstall my os once sometime after downloaded this, might be holding onto old IDs or something, a fresh download will rebuild the data with current IDs

--- lu/build.sh
+++ lu/build.sh
@@ -1,4 +1,3 @@
-m=-
 DRIVE=$(stat -c %m $0)
 ALL=${DRIVE}/common
 exists() {
@@ -21,39 +20,47 @@ chkdir() {
 }
 
 prep_gcc() {
+	local f="-Wall -Wextra -Werror"
+	local p=""
 	if [ "$1" = "64" ]; then
 		ARCH=amd64
 	elif [ "$1" = "32" ]; then
 		ARCH=i386
 	fi
 	if [ "$2" = "mgw" ]; then
-		_FLAGS=$mWall $mWerror $mmconsole $mmwindows $mmwin32
+		if [ "$3" = "Windows" ]; then
+			f="${f} -mconsole -mwindows -mwin32"
+		fi
 		if [ "$1" = "64" ]; then
-			PFX=x86_64-w64-mingw32-
-			LFLAGS=-m64
-			CFLAGS=-m64
+			p="x86_64-w64-mingw32-"
+			LFLAGS="-m64"
+			CFLAGS="-m64"
 		elif [ "$1" = "32" ]; then
-			PFX=i386-w64-mingw32-
-			LFLAGS=-m32
-			CFLAGS=-m32
+			p="i386-w64-mingw32-"
+			LFLAGS="-m32"
+			CFLAGS="-m32"
 		fi
 	fi
-	LD=${PFX}gcc
-	CC=${PFX}gcc
-	CXX=${PFX}g++
-	LFLAGS=${_FLAGS} ${LFLAGS}
-	CFLAGS=${_FLAGS} ${CFLAGS}
-	O_DIR=${PFX}gcc
+	LD="${p}gcc"
+	CC="${p}gcc"
+	CXX="${p}g++"
+	LFLAGS="${f} ${LFLAGS}"
+	CFLAGS="${f} ${CFLAGS}"
+	O_DIR="${p}gcc"
 }
 prep_gcc 64
+compile() {
+	local c="${CC} ${CFLAGS} -o \"$2\" -c \"$3\""
+	echo "$1: $c"
+	$c
+}
 compile_obj() {
-	local c="${CC} ${CFLAGS} -o $1 -c $2"
-	echo $c
+	
 	# Add object to list
 	OBJS="${OBJS} $1"
 	# Check if we should compile the object
 	if [ $(missing $1) ]; then
-		$c
+		compile "Missing object" $1 $2
 		return 0
 	fi
 	# Force skip
@@ -61,20 +68,22 @@ compile_obj() {
 	fi
 	# Force Compile
 	if [ "$3" = "true" ]; then
-		$c
+		compile "Forced" $1 $2
 		return 0
 	fi
 	# Check if up to date
+	local d=""
+	local s=""
 	if uname | grep -q "Darwin"; then
-		local d=$(stat -f %m $1)
-		local s=$(stat -f %m $2)
+		d=$(stat -f %m $1)
+		s=$(stat -f %m $2)
 	else
-		local d=$(stat -c %Y $1)
-		local s=$(stat -c %Y $2)
+		d=$(stat -c %Y $1)
+		s=$(stat -c %Y $2)
 	fi
 	echo Object: $d vs Source: $s
 	if [ "$d" -ne "$s" ]; then
-		$c
+		compile "Modified" $1 $2
 		return 0
 	fi
 	return 0
@@ -106,8 +115,10 @@ system() {
 }
 system
 
+#PRJ_DIR=$(pwd)/
+SRC_DIR=${PRJ_DIR}
 OUT_DIR="${ALL}/lu"
-OBJ_DIR=objs
+OBJ_DIR=${PRJ_DIR}objs
 chkdir "${OBJ_DIR}"
 OBJ_DIR="${OBJ_DIR}/${O_DIR}"
 
@@ -115,16 +126,16 @@ LUA_DIR="${ALL}/lua-5.3.5"
 LUA_INC="${LUA_DIR}/src"
 OBJ_LUA_DIR="${OBJ_DIR}/lua"
 CPATH=$(add_path "${LUA_INC}" "${CPATH}")
-LPATH=$(add_path "${LUA_INC}" "${LPATH}")
+LPATH=$(add_path "${LUA_DIR}" "${LPATH}")
 paths
 
 echo Compiling Lu Objects
 chkdir "${OBJ_DIR}"
 chknam(){
 	n=$(basename $1)
-	compile_obj "${OBJ_LUA_DIR}/$n.o" $1 true
+	compile_obj "${OBJ_DIR}/$n.o" $1 true
 }
-for i in *.c
+for i in "${SRC_DIR}"*.c
 do
 	chknam "$i"
 done
